c4a7914cbff72df50d3b11dbfd24fc346e59a612
postmodern <postmodern.mod3@gmail.com>
2010-02-18 18:05:38 -0800

    Spec wording change.

--- spec/parameter_handler_spec.rb
+++ spec/parameter_handler_spec.rb
@@ -12,19 +12,19 @@ describe "ParameterHandler" do
       parse_file :simple_parameter
     end
 
-    it "should define a class reader method for the parameter" do
+    it "should register a class reader method for the parameter" do
       yard('SimpleParameter.x').should be_instance_of(CodeObjects::MethodObject)
     end
 
-    it "should define a class writer method for the parameter" do
+    it "should register a class writer method for the parameter" do
       yard('SimpleParameter.x=').should be_instance_of(CodeObjects::MethodObject)
     end
 
-    it "should define a instance reader method for the parameter" do
+    it "should register a instance reader method for the parameter" do
       yard('SimpleParameter#x').should be_instance_of(CodeObjects::MethodObject)
     end
 
-    it "should define a instance writer method for the parameter" do
+    it "should register a instance writer method for the parameter" do
       yard('SimpleParameter#x=').should be_instance_of(CodeObjects::MethodObject)
     end
   end
@@ -34,19 +34,19 @@ describe "ParameterHandler" do
       parse_file :bad_parameter
     end
 
-    it "should not define class reader methods for variables" do
+    it "should not register class reader methods for variables" do
       yard('BadParameter.x').should be_nil
     end
 
-    it "should not define class writer methods for variables" do
+    it "should not register class writer methods for variables" do
       yard('BadParameter.x=').should be_nil
     end
 
-    it "should not define instance reader methods for variables" do 
+    it "should not register instance reader methods for variables" do 
       yard('BadParameter#x').should be_nil
     end
 
-    it "should not define instance writer methods for variables" do
+    it "should not register instance writer methods for variables" do
       yard('BadParameter#x=').should be_nil
     end
   end
