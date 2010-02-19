ae06fdbe249ae8eb78f36d57a674f64673a2dcaa
postmodern <postmodern.mod3@gmail.com>
2010-02-18 18:00:32 -0800

    Separated 'simple_parameter.rb.txt' specs from 'bad_parameter.rb.txt' specs.
    * This is because parse_file clears the YARD Registry.

--- spec/parameter_handler_spec.rb
+++ spec/parameter_handler_spec.rb
@@ -7,31 +7,38 @@ require 'yard-parameters/legacy/parameter_handler'
 describe "ParameterHandler" do
   include Helpers::Examples
 
-  before(:all) do
-    parse_file :simple_parameter
-    parse_file :bad_parameter
+  describe "valid" do
+    before(:all) do
+      parse_file :simple_parameter
+    end
+
+    it "should define a class reader method for the parameter" do
+      yard('SimpleParameter.x').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define a class writer method for the parameter" do
+      yard('SimpleParameter.x=').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define a instance reader method for the parameter" do
+      yard('SimpleParameter#x').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define a instance writer method for the parameter" do
+      yard('SimpleParameter#x=').should be_instance_of(CodeObjects::MethodObject)
+    end
   end
 
-  it "should define a class reader method for the parameter" do
-    yard('SimpleParameter.x').should be_instance_of(CodeObjects::MethodObject)
-  end
+  describe "invalid" do
+    before(:all) do
+      parse_file :bad_parameter
+    end
 
-  it "should define a class writer method for the parameter" do
-    yard('SimpleParameter.x=').should be_instance_of(CodeObjects::MethodObject)
-  end
-
-  it "should define a instance reader method for the parameter" do
-    yard('SimpleParameter#x').should be_instance_of(CodeObjects::MethodObject)
-  end
-
-  it "should define a instance writer method for the parameter" do
-    yard('SimpleParameter#x=').should be_instance_of(CodeObjects::MethodObject)
-  end
-
-  it "should not define methods for variables named 'parameter'" do
-    yard('BadParameter.x').should be_nil
-    yard('BadParameter.x=').should be_nil
-    yard('BadParameter#x').should be_nil
-    yard('BadParameter#x=').should be_nil
+    it "should not define methods for variables named 'parameter'" do
+      yard('BadParameter.x').should be_nil
+      yard('BadParameter.x=').should be_nil
+      yard('BadParameter#x').should be_nil
+      yard('BadParameter#x=').should be_nil
+    end
   end
 end
